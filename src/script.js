b0b98ff75f11499d3e732528eb97e9673fd10f62
Keerthivasan K <keerthianbu2000@gmail.com>
2021-06-29 17:55:35 +0530

    bug fix

--- src/script.js
+++ src/script.js
@@ -18,7 +18,7 @@ function delay(n) {
 function contentAnimation() {
     gsap.from('#app', { duration: 1, opacity: 0, scale: 1.2 });
     gsap.from('.header', { duration: 1, opacity: 0, y: -100, delay: 0.5 });
-    gsap.from('.gsap', { duration: 1, opacity: 0, x: -50, delay: 1});
+    gsap.from('.gsap', { duration: 1, opacity: 0, y: 50, delay: 1});
     gsap.from('.image', { duration: 1, opacity: 0, delay: 1.2});
     // gsap.from('.text h2', { duration: 1, opacity: 0, x: -100, delay: 1 });
     // gsap.from('.text h1', { duration: 1, opacity: 0, delay: 1.5 });
