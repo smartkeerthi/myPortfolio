6035c64e78b9aa12263dfd4eecc419e0d6b33d1f
Keerthivasan K <keerthianbu2000@gmail.com>
2021-07-04 14:18:14 +0530

    new design

--- src/script.js
+++ src/script.js
@@ -1,14 +1,2 @@
 
-
-gsap.registerPlugin(ScrollTrigger);
-
-gsap.utils.toArray('.gsapSection').forEach(section => {
-    ScrollTrigger.create({
-        trigger: section,
-        start: 'top top',
-        pin: true,
-        pinSpacing: false
-    });
-});
-
-gsap.from('.section', { duration: 1.5, opacity: 0, x: -50 });
+AOS.init();
