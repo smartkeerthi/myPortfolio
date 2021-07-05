1ccf68f9a3b7488d5756fcc9f4cc63d4118b74c3
Keerthivasan K <keerthianbu2000@gmail.com>
2021-07-05 17:31:55 +0530

    added new

--- src/script.js
+++ src/script.js
@@ -1,6 +1,11 @@
 
 AOS.init();
 
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
 root = document.querySelector(':root')
 var themeName;
 
@@ -22,14 +27,24 @@ function changeTheme(color) {
         root.style.setProperty('--accent-color-2', '#1a508b')
     }else if(color == 'blue') {
         root.style.setProperty('--primary-color', '#b5eaea')
-        root.style.setProperty('--secondary-color', '#f38ba0')
+        root.style.setProperty('--secondary-color', '#0a1931')
         root.style.setProperty('--accent-color-1', '#ffbcbc')
-        root.style.setProperty('--accent-color-2', '#0a1931')
+        root.style.setProperty('--accent-color-2', '#f38ba0')
     }else if(color == 'dark') {
         root.style.setProperty('--primary-color', '#0a1931')
         root.style.setProperty('--secondary-color', '#efefef')
         root.style.setProperty('--accent-color-1', '#185adb')
         root.style.setProperty('--accent-color-2', '#ffc947')
+    }else if(color == 'darkBlue') {
+        root.style.setProperty('--primary-color', '#364f6b')
+        root.style.setProperty('--secondary-color', '#f5f5f5')
+        root.style.setProperty('--accent-color-1', '#3fc1c9')
+        root.style.setProperty('--accent-color-2', '#fc5185')
+    }else if(color == 'purple') {
+        root.style.setProperty('--primary-color', '#4f3961')
+        root.style.setProperty('--secondary-color', '#f3d4d4')
+        root.style.setProperty('--accent-color-1', '#fc9d9d')
+        root.style.setProperty('--accent-color-2', '#ea728c')
     }
 }
 
