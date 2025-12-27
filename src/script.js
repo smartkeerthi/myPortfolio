86ba795acc842d7b82551c5f1861b9a338df261f
Keerthivasan K <keerthianbu2000@gmail.com>
2025-12-27 20:13:24 +0530

    Portfolio2.0
    First commit of new portfolio look

--- src/script.js
+++ src/script.js
@@ -1,54 +1,173 @@
 
-AOS.init();
+document.addEventListener("DOMContentLoaded", () => {
+    gsap.registerPlugin(CustomEase, SplitText, ScrollTrigger);
+    CustomEase.create("hop", ".8, 0, .3, 1");
 
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
-})
+    const splitTextElements = (selector, type = "words, chars", addFirstChar = false) => {
+        const elements = document.querySelectorAll(selector)
+        elements.forEach((element) => {
+            const splitText = new SplitText(element, {
+                type, wordsClass: "word", charsClass: "char"
+            });
 
-root = document.querySelector(':root')
-var themeName;
+            if (type.includes("chars")) {
+                splitText.chars.forEach((char, index) => {
+                    const originalText = char.textContent;
+                    char.innerHTML = `<span>${originalText}</span>`;
 
-window.addEventListener('load', () => {
-    const themes = localStorage.getItem('theme')
-    if(themes == null) {
-        localStorage.setItem('theme', 'dark')
-        changeTheme('dark')
-    }else {
-        changeTheme(themes)
+                    if (addFirstChar && index == 0) {
+                        char.classList.add("first-char");
+                    }
+
+                    if (addFirstChar && index == 12) {
+                        char.classList.add("first-char-1");
+                    }
+                });
+            }
+        });
     }
-})
 
-function changeTheme(color) {
-    if(color == 'white') {
-        root.style.setProperty('--primary-color', '#fff3e6')
-        root.style.setProperty('--secondary-color', '#0d335d')
-        root.style.setProperty('--accent-color-1', '#c1a1d3')
-        root.style.setProperty('--accent-color-2', '#1a508b')
-    }else if(color == 'blue') {
-        root.style.setProperty('--primary-color', '#b5eaea')
-        root.style.setProperty('--secondary-color', '#0a1931')
-        root.style.setProperty('--accent-color-1', '#ffbcbc')
-        root.style.setProperty('--accent-color-2', '#f38ba0')
-    }else if(color == 'dark') {
-        root.style.setProperty('--primary-color', '#0a1931')
-        root.style.setProperty('--secondary-color', '#efefef')
-        root.style.setProperty('--accent-color-1', '#185adb')
-        root.style.setProperty('--accent-color-2', '#ffc947')
-    }else if(color == 'darkBlue') {
-        root.style.setProperty('--primary-color', '#364f6b')
-        root.style.setProperty('--secondary-color', '#f5f5f5')
-        root.style.setProperty('--accent-color-1', '#3fc1c9')
-        root.style.setProperty('--accent-color-2', '#fc5185')
-    }else if(color == 'purple') {
-        root.style.setProperty('--primary-color', '#4f3961')
-        root.style.setProperty('--secondary-color', '#f3d4d4')
-        root.style.setProperty('--accent-color-1', '#fc9d9d')
-        root.style.setProperty('--accent-color-2', '#ea728c')
-    }
-}
+    const isMobile = window.innerWidth <= 680;
 
-function setTheme(colorName) {
-    localStorage.setItem('theme', colorName)
-    changeTheme(colorName)
-}
+    splitTextElements(".intro-title", "words, chars", true);
+
+    gsap.set([".split-overlay .intro-title .first-char span", ".split-overlay .intro-title .first-char-1 span"], { y: "0%" })
+    gsap.set(".split-overlay .intro-title .first-char", {
+        x: isMobile ? "9rem" : "17.5rem"
+    });
+    gsap.set(".split-overlay .intro-title .first-char-1", {
+        x: isMobile ? "-1.5rem" : "-2.5rem"
+    });
+
+    const tl = gsap.timeline({ defaults: { ease: "hop" } })
+    tl.to(".preloader .intro-title .char span", {
+        y: "0%",
+        opacity: 1,
+        duration: 0.75,
+        stagger: 0.05
+    }, 0.5)
+        .to(".preloader .intro-title .char:not(.first-char, .first-char-1) span", {
+            y: "100%",
+            opacity: 0,
+            duration: 0.75,
+            stagger: 0.05
+        }, 2)
+        .to(".first-char", {
+            x: isMobile ? "9rem" : "17.5rem",
+            duration: 0.75
+        }, 3)
+        .to(".first-char-1", {
+            x: isMobile ? "-1.5rem" : "-2.5rem",
+            duration: 0.75,
+            onComplete: () => {
+                gsap.set(".preloader", {
+                    clipPath: "polygon(0 0, 100% 0, 100% 50%, 0 50%)"
+                })
+                gsap.set(".split-overlay", {
+                    clipPath: "polygon(0 50%, 100% 50%, 100% 100%, 0 100%)"
+                })
+            }
+        }, 3)
+
+    tl.to([".preloader", ".split-overlay"], {
+        y: (i) => (i == 0 ? "-50%" : "50%"),
+        duration: 1
+    }, 4)
+        .to(".split-overlay", {
+            opacity: 0,
+            duration: 1
+        }, 4.5)
+
+    tl.to([".navbar", ".mobile-navbar"], {
+        y: 0,
+        opacity: 1,
+        duration: 0.75
+    }, 4.5)
+        .to(".intro p", {
+            opacity: 1,
+            duration: 0.75
+        }, 5)
+        .to([".intro h2 span", ".intro-scrolldown svg path"], {
+            y: 0,
+            duration: 0.75
+        }, 5)
+        .to(".social-links ul li a", {
+            x: 0,
+            opacity: 1,
+            duration: 0.75
+        }, 5)
+
+    const marqueeContainer = document.querySelectorAll(".marquee-container");
+
+    marqueeContainer.forEach((container, index) => {
+        let start = "0%"
+        let end = "-15%"
+
+        if (index % 2 === 0) {
+            start = "0%"
+            end = "10%"
+        }
+
+        const marquee = container.querySelector(".marquee")
+
+        gsap.fromTo(
+            marquee,
+            {
+                x: start
+            },
+            {
+                x: end,
+                scrollTrigger: {
+                    trigger: container,
+                    start: "top bottom",
+                    end: "150% top",
+                    scrub: true
+                }
+            })
+    })
+
+    const skills = gsap.utils.toArray(".description");
+
+    const t1 = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".skills",
+            start: "top 8.5%",
+            end: () => `+=${skills.length * 50}%`,
+            scrub: true,
+            pin: true,
+        }
+    });
+
+    skills.forEach((skill, i) => {
+        if (i === 0) return;
+
+        t1.to(skill, {
+            yPercent: `-${(90) * i}`,
+            duration: 1
+        }, i - 1);
+    });
+
+    const cards = gsap.utils.toArray(".stacking-card");
+
+    const t2 = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".projects",
+            start: "top top",
+            end: () => `+=${cards.length * 50}%`,
+            scrub: true,
+            pin: true
+        }
+    });
+
+    cards.forEach((card, i) => {
+        // if (i === 0) return;
+
+        t2.to(card, {
+            yPercent: `-${(115) * i}`,
+            scale: 0.9,
+            duration: 1
+        }, i - 1);
+    });
+
+
+});
