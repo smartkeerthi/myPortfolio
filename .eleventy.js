23905485f55b7baa7c8a70636cc4ad60e756393b
Keerthivasan K <keerthianbu2000@gmail.com>
2026-01-22 11:09:16 +0530

    added redirects

--- .eleventy.js
+++ .eleventy.js
@@ -9,6 +9,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/assets');
   eleventyConfig.addPassthroughCopy('./src/favicon');
   eleventyConfig.addPassthroughCopy('./src/admin');
+  eleventyConfig.addPassthroughCopy('./src/_redirects');
 
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
